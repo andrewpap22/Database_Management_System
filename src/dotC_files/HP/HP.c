86cf127faa3c6fd86e7a0fb28254d2388de747c3
andrewpap22 <andrewpap1997@gmail.com>
2020-11-29 11:48:22 +0200

    finish HP_OpenFile :white_check_mark:

--- src/dotC_files/HP/HP.c
+++ src/dotC_files/HP/HP.c
@@ -47,7 +47,7 @@ int HP_CreateFile(char *fileName, char attrType, char *attrName, int attrLength)
     return -1;
   }
 
-  if(BF_CloseFile(info.fileDesc) < 0)
+  if (BF_CloseFile(info.fileDesc) < 0)
   {
     BF_PrintError("[!] Error in closing the file in HP_CreateFile");
     return -1;
@@ -58,25 +58,41 @@ int HP_CreateFile(char *fileName, char attrType, char *attrName, int attrLength)
 
 HP_info *HP_OpenFile(char *fileName)
 {
+  HP_info *info;
+  void *block;
 
+  info = malloc(sizeof(HP_info));
+
+  if ((info->fileDesc = BF_OpenFile(fileName)) < 0)
+  {
+    BF_PrintError("[!] Error in opening the heap file in HP_OpenFile");
+    exit(1);
+  }
+
+  if (BF_ReadBlock(BF_OpenFile(fileName), 0, &block) < 0)
+  {
+    BF_PrintError("[!] Error in reading the block in HP_OpenFile");
+    BF_CloseFile(info->fileDesc);
+    exit(1);
+  }
+
+  memcpy(info, block, sizeof(HP_info));
+
+  return info;
 }
 
 int HP_CloseFile(HP_info *header_info)
 {
-
 }
 
 int HP_InsertEntry(HP_info header_info, Record record)
 {
-
 }
 
 int HP_DeleteEntry(HP_info header_info, void *value)
 {
-
 }
 
 int HP_GetAllEntries(HP_info header_info, void *value)
 {
-
 }
